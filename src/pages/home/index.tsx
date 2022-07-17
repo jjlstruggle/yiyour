ba8721547dd08d257492fd7b18761243644a9b43
jjlstruggle <2020213994@stu.cqupt.edu.cn>
2022-07-17 23:07:19 +0800

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,16 +2,35 @@
  * 任务集市&作品商店
  */
 import "./index.css";
-import { Carousel, Tabs } from "antd";
+import { Carousel, Tabs, Dropdown, Menu, Button } from "antd";
 import img from "@/assets/temp/shell.jpg";
 import useLazy from "@/hooks/useLazy";
+import { Fragment, useRef, useState } from "react";
+import { DownOutlined } from "@ant-design/icons";
+
 const imgs = [img, img, img];
 const { TabPane } = Tabs;
 const Bazaar = useLazy(import("@/components/home/bazaar"));
+
+const sorts = ["时间降序", "时间增序"];
+
 const onChange = (key: string) => {
   console.log(key);
 };
 export default function Index() {
+  const [select, setSelect] = useState(0);
+  const menu = useRef(
+    <Menu
+      onClick={({ key }) => {
+        setSelect(Number(key));
+      }}
+      items={sorts.map((item, index) => ({
+        key: index,
+        label: item,
+      }))}
+    />
+  ).current;
+
   return (
     <div>
       <div className="bg-main flex flex-col items-center w-full py-8">
@@ -36,6 +55,20 @@ export default function Index() {
           size="large"
           tabBarGutter={96}
           className="text-xl"
+          tabBarExtraContent={
+            <Fragment>
+              <Dropdown
+                overlay={menu}
+                trigger={["click"]}
+                children={
+                  <Button type="primary" className="flex items-center">
+                    {sorts[select]}
+                    <DownOutlined />
+                  </Button>
+                }
+              />
+            </Fragment>
+          }
         >
           <TabPane tab="任务集市" key="1">
             <Bazaar />
