454cedfc9027f44ca94be96149d1c137943fc30c
秦海峰cq <9802591+qin-haifeng-cq@user.noreply.gitee.com>
2022-07-21 22:35:35 +0800

    作品详细

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-    corePlugins: {},
-    content: [
-        "./src/**/*.{ts,tsx,less}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                'main': '#8A5DC4',
-            },
-        },
+  corePlugins: {},
+  content: ["./src/**/*.{ts,tsx,less}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#8A5DC4",
+      },
     },
-    plugins: [require('@tailwindcss/line-clamp'),],
-    important: true,
-}
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+  important: true,
+};
