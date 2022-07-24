8f0e0f7cb52387889f98ff6bb243935cac389792
秦海峰cq <9802591+qin-haifeng-cq@user.noreply.gitee.com>
2022-07-24 17:59:42 +0800

    个人信息

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,7 +1,17 @@
+import useLazy from "@/hooks/useLazy";
+const Header =useLazy(import("../../components/user/header"))
+
 /**
  * 个人信息页
  */
 
 export default function Users() {
-  return <div>个人信息页</div>;
+  return (<div style={{
+    backgroundColor: "#F7F7F7",
+    height: "100vh",
+    padding:"4vh 5vw"
+  }} >
+           <Header />
+  </div>)
+
 }
