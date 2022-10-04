2178dc3f7467f3cc49a4cc8aab888637bbdb8c77
秦海峰cq <9802591+qin-haifeng-cq@user.noreply.gitee.com>
2022-10-04 11:01:58 +0800

    背景

--- src/pages/account/login/index.tsx
+++ src/pages/account/login/index.tsx
@@ -43,6 +43,7 @@ export default function Login() {
             hasLogin: true,
             userInfo: res.data,
           });
+          navigate("/home");
         } else if (res.code == "1000") {
           setPsworde({ e: true, t: res.msg });
         } else {
